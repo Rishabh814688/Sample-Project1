70ebaa0bffc7c8ff29ac9b39a37a2c7114ce3d21
Your Name <you@example.com>
2024-01-09 05:29:49 -0800

    python file

---
+++ SamplePyProgram.py
@@ -0,0 +1,18 @@
+Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> import os
+>>> List1=[8,9,3,1,10]
+>>> List1.reverse()
+>>> print('The reverse List is', List1)
+The reverse List is [10, 1, 3, 9, 8]
+>>> List2=[3,6,1,7,3,7,3,2,1]
+>>> List2.sort()
+>>> print('The sorted list is ', List2)
+The sorted list is  [1, 1, 2, 3, 3, 3, 6, 7, 7]
+>>> List3 = [3,5,7,8,9,1,4,2,6,]
+>>> List3=List3.copy()
+>>> print('Copying the List',List3)
+Copying the List [3, 5, 7, 8, 9, 1, 4, 2, 6]
+>>> indexvalue = List3[2:6]
+>>> print('The index value are',indexvalue)
+The index value are [7, 8, 9, 1]
